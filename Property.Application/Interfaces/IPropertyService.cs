5688fe155f993786d45e1237f8add62d065dee5c
jhon2201 <53099030+jhon2201@users.noreply.github.com>
2021-07-21 12:56:00 -0500

    Add files via upload

---
+++ Property.Application/Interfaces/IPropertyService.cs
@@ -0,0 +1,15 @@
+﻿using Property.Application.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Property.Application.Interfaces
+{
+    public interface IPropertyService
+    {
+        PropertyViewModel GetPropertys();
+        //IEnumerable<PropertyViewModel> GetPropertys();
+    }
+}
